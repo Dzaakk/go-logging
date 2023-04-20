ca0bdc548f49487dea7448135b7f558e9bb743a4
Dzaky Mohammad <63341994+Dzaakk@users.noreply.github.com>
2023-04-21 00:01:22 +0700

    logger

---
+++ logger_test.go
@@ -0,0 +1,15 @@
+package gologging
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+)
+
+func TestLogger(t *testing.T) {
+	logger := logrus.New()
+
+	logger.Println("Helllo Logger")
+	fmt.Println("Hello Logger")
+}
