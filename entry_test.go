3a5a30a429a309ec18ae93e9c1eb8704c5a7ac5b
Dzaky Mohammad <63341994+Dzaakk@users.noreply.github.com>
2023-04-21 00:33:47 +0700

    entry

---
+++ entry_test.go
@@ -0,0 +1,16 @@
+package gologging
+
+import (
+	"testing"
+
+	"github.com/sirupsen/logrus"
+)
+
+func TestEntry(t *testing.T) {
+	logger := logrus.New()
+	logger.SetFormatter(&logrus.JSONFormatter{})
+
+	entry := logrus.NewEntry(logger)
+	entry.WithField("username", "user1")
+	entry.Info("Hello Entry")
+}
